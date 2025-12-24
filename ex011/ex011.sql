306a6795e304da49438e9d50244a0a37ddc7cf98
OYanEnrique <enrique4work@gmail.com>
2025-12-24 14:54:07 -0300

    feat: adiciona ex011 - INSERT INTO múltiplos registros na tabela de clientes

---
+++ ex011/ex011.sql
@@ -0,0 +1,6 @@
+INSERT INTO tabelaclientes 
+(id_cliente, nome_cliente, informacoes_de_contato, endereco_cliente)
+VALUES 
+(2, 'Maria Oliveira', 'maria.oliveira@email.com', 'Avenida Central, 456'),
+(3, 'Carlos Pereira', 'carlos.pereira@email.com', 'Praça da Sé, 789'),
+(4, 'Ana Souza', 'ana.souza@email.com', 'Rua das Acácias, 101');
