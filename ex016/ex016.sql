abff6a2024cfa3923bed05aa25f6e0abbd2de1db
OYanEnrique <enrique4work@gmail.com>
2025-12-25 15:28:16 -0300

    feat: adiciona ex016 - ORDER BY (ASC e DESC)

---
+++ ex016/ex016.sql
@@ -0,0 +1,3 @@
+SELECT * FROM tabelaprodutos WHERE preco_de_compra BETWEEN 200 AND 600 ORDER BY nome_do_produto;
+SELECT * FROM tabelaprodutos WHERE preco_de_compra BETWEEN 200 AND 600 ORDER BY data_de_inclusao;
+SELECT * FROM tabelaprodutos WHERE preco_de_compra BETWEEN 200 AND 600 ORDER BY fornecedor DESC;
